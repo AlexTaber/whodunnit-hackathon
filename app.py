531e4a5ff43f36f669378aec50181653e2174d49
alextaber <alex.taber0@gmail.com>
2022-04-30 08:01:37 -0400

    remove references to rooms from app

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from game import Game
 from game.state import game_state
-from rooms.model import bathroom, engineering_table, kitchen
 # from scenes.title import TitleScene
+from rooms.model import init_rooms
 
-game_state.rooms.append(kitchen)
-game_state.rooms.append(engineering_table)
-game_state.rooms.append(bathroom)
-
+init_rooms()
 game = Game(game_state)
 game.run(game_state.rooms[0].scene)
--- rooms/model.py
+++ rooms/model.py
@@ -1,4 +1,5 @@
 from typing import Union
+from game.state import game_state
 from interactions.model import Interaction
 from persons.model import dan_b, dan_e, dan_i, dan_s, dee, joy
 from interactions.model import Interaction
@@ -23,6 +24,8 @@ class Room:
 
         self.scene: Union[Scene, None] = None
 
+        game_state.rooms.append(self)
+
     def get_names(self) -> list[str]:
         return [interaction.person.name for interaction in self.interactions]
 
@@ -31,48 +34,50 @@ class Room:
             if interaction.person.name == name:
                 return interaction.scene
 
-kitchen = add_room(Room(
-    name="Kitchen",
-    description="You walk into the kitchen. The sink is filled with dirty coffee cups. There's White Claw  & LaCroix cans overflowing the recycling bin. Dan I & Dan S are trying to figure out how to make coffee.",
-    interactions=[
-        Interaction(
-            person=dan_s,
-            scene=DanSInterviewScene(),
-        ),
-        Interaction(
-            person=dan_i,
-            scene=DanIInterviewScene(),
-        ),
-    ],
-), KitchenScene)
 
-engineering_table = add_room(Room(
-    name="Engineering Table",
-    description="You walk over to the Engineering Table. Dee and Joy are at the table discussing how to best implement Andrea's hopes & desires for the Candidate flow.",
-    interactions=[
-        Interaction(
-            person=joy,
-            scene=JoyInterviewScene(),
-        ),
-        Interaction(
-            person=dee,
-            scene=DeeInterviewScene(),
-        ),
-    ],
-), EngineeringTableScene)
+def init_rooms():
+    add_room(Room(
+        name="Kitchen",
+        description="You walk into the kitchen. The sink is filled with dirty coffee cups. There's White Claw  & LaCroix cans overflowing the recycling bin. Dan I & Dan S are trying to figure out how to make coffee.",
+        interactions=[
+            Interaction(
+                person=dan_s,
+                scene=DanSInterviewScene(),
+            ),
+            Interaction(
+                person=dan_i,
+                scene=DanIInterviewScene(),
+            ),
+        ],
+    ), KitchenScene)
 
-bathroom = add_room(Room(
-    name="Bathroom",
-    description="You make your way to the bathroom. In the hall you see Lauren digging for something in the closet, boxes strewn everywhere. Daniel E & Dan B are standing in line to use the bathroom. Too much coffee for everyone this morning.",
-    interactions=[
-        Interaction(
-            person=dan_e,
-            scene=DanielEInterviewScene(),
-        ),
-        Interaction(
-            person=dan_b,
-            scene=DanBInterview(),
-        ),
-    ],
-), BathroomScene)
+    add_room(Room(
+        name="Engineering Table",
+        description="You walk over to the Engineering Table. Dee and Joy are at the table discussing how to best implement Andrea's hopes & desires for the Candidate flow.",
+        interactions=[
+            Interaction(
+                person=joy,
+                scene=JoyInterviewScene(),
+            ),
+            Interaction(
+                person=dee,
+                scene=DeeInterviewScene(),
+            ),
+        ],
+    ), EngineeringTableScene)
+
+    add_room(Room(
+        name="Bathroom",
+        description="You make your way to the bathroom. In the hall you see Lauren digging for something in the closet, boxes strewn everywhere. Daniel E & Dan B are standing in line to use the bathroom. Too much coffee for everyone this morning.",
+        interactions=[
+            Interaction(
+                person=dan_e,
+                scene=DanielEInterviewScene(),
+            ),
+            Interaction(
+                person=dan_b,
+                scene=DanBInterview(),
+            ),
+        ],
+    ), BathroomScene)
 
