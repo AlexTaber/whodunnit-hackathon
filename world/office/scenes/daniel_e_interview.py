da4808bfbf198ce4a4b4bfcdc05050fca95b29ea
alextaber <alex.taber0@gmail.com>
2022-05-01 18:09:00 -0400

    remove extra space from daniel e interview

--- world/office/scenes/daniel_e_interview.py
+++ world/office/scenes/daniel_e_interview.py
@@ -6,7 +6,7 @@ from state import game_state
 class DanielEInterviewScene(Scene):
     def run(self):
         print(
-            "[secondary]Let me tell you about my morning.  I met up with Dee to determine which is faster way to get to work, my mountain bike or his motorcycle. It was a very close race but without a doubt my bike is faster and more efficient."
+            "[secondary]Let me tell you about my morning. I met up with Dee to determine which is faster way to get to work, my mountain bike or his motorcycle. It was a very close race but without a doubt my bike is faster and more efficient."
         )
         print("[secondary]Or maybe it's just me who is faster!")
         return game_state.previous_scene
