e58a39bede3d7f0f3d4bab80efca9ab49380834c
Michael Anderson <michael.anderson@ripplematch.com>
2022-05-03 12:35:01 -0400

    Add fishing. Add fishing-related endings. Misc typing cleanups.

--- engine/world/room_action.py
+++ engine/world/room_action.py
@@ -1,8 +1,9 @@
-from typing import Union
+from typing import Callable, Union
+
 from engine.scene import Scene
 
 
 class RoomAction:
-    def __init__(self, name: str, scene: Union[Scene, callable]):
+    def __init__(self, name: str, scene: Union[Scene, Callable]):
         self.name = name
         self.scene = scene
---
+++ fishing/reel_input.py
@@ -0,0 +1,7 @@
+from engine.printer.oscilating_input import get_input_from_oscilating_meter
+
+
+def get_reel_power(max_power: int = 10) -> int:
+    """Get the reel strength from the user."""
+    print("Press ENTER to reel in the fish!")
+    return get_input_from_oscilating_meter("Reel power", max_power)
